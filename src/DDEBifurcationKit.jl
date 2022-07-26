4ece4c2179e456ee15b484f35fe7c9991292f57f
Romain Veltz <romain.veltz@inria.fr>
2022-07-26 08:35:24 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/DDEBifurcationKit.jl
@@ -0,0 +1,5 @@
+module DDEBifurcationKit
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DDEBifurcationKit
+using Test
+
+@testset "DDEBifurcationKit.jl" begin
+    # Write your tests here.
+end
