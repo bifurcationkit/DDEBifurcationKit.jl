5689d5e3c6ab83dbe6e725037000d31b856661c4
romain veltz <romain.veltz@inria.fr>
2022-12-17 18:58:43 +0100

    update main module
    add problems.jl

--- src/DDEBifurcationKit.jl
+++ src/DDEBifurcationKit.jl
@@ -1,5 +1,18 @@
 module DDEBifurcationKit
 
-# Write your package code here.
+	using BifurcationKit, Setfield, DocStringExtensions, RecursiveArrayTools
+	using ForwardDiff, Parameters, LinearAlgebra
+	using NonlinearEigenproblems
+	const BK = BifurcationKit
+
+	abstract type AbstractDDEBifurcationProblem <: BK.AbstractBifurcationProblem end
+
+	include("problems.jl")
+	include("diffeq.jl")
+	include("EigSolver.jl")
+	include("codim2/codim2.jl")
+	include("codim2/Hopf.jl")
+
+
 
 end
