4f15598f57d6142ef784cd23bbc1cec7e52bb68f
romain veltz <romain.veltz@inria.fr>
2026-03-15 09:51:55 +0100

    test: structure runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,19 @@ using DDEBifurcationKit
 using Test
 
 @testset "DDEBifurcationKit.jl" begin
-    include("normalform.jl")
-    include("codim2.jl")
-    include("pocoll.jl")
-    include("pocoll-sdde.jl")
+    @testset "Normal forms" begin
+        include("normalform.jl")
+    end
+    @testset "Codim 2" begin
+        include("codim2.jl")
+    end
+    @testset "Collocation DDE" begin
+        include("pocoll.jl")
+    end
+    @testset "Collocation SD-DDE" begin
+        include("pocoll-sdde.jl")
+    end
+    @testset "Floquet" begin
+        include("testfloquet.jl")
+    end
 end
